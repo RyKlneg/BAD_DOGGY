d3569f43a963138f6a6b2b9b02ab4839815b42a9
RyKlneg <zabe080803@gmail.com>
2025-11-03 13:12:43 -0600

    Section Nosotros

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,6 +4,9 @@
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     @vite('resources/css/app.css')
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Fascinate&display=swap');
+    </style>
   </head>
   <body>
     <script src="https://cdn.jsdelivr.net/npm/flowbite@3.1.2/dist/flowbite.min.js"></script>
@@ -48,7 +51,7 @@
             },
 
             startCarousel() {
-            this.interval = setInterval(() => this.changeImage(), 10000);
+            this.interval = setInterval(() => this.changeImage(), 20000);
             }
         }"
         x-init="startCarousel"
@@ -74,14 +77,43 @@
         <!-- Botón manual -->
         <button
             @click="changeImage()"
-            class="absolute right-4 top-1/2 transform -translate-y-1/2 bg-white bg-opacity-50 p-2 rounded-full hidden md:block hover:bg-opacity-75 transition"
+            class="absolute right-4 top-1/2 transform -translate-y-1/2 bg-white bg-opacity-50 p-2 rounded-full hidden md:block hover:bg-amber-300 transition"
         >
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
             <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
             </svg>
         </button>
     </section>
-    <section id="Nosotros" style="background-image:url('{{ asset('images/Nosotros.png') }}')" role="img" aria-label="Nosotros" class="bg-cover bg-center w-full bg-black h-48 sm:h-64 md:h-96 lg:h-screen"></section>
+    <section id="Nosotros" style="background-image:url('{{ asset('images/Nosotros.png') }}')" role="img" aria-label="Nosotros" class="bg-cover bg-center w-full bg-black h-64 sm:h-80 md:h-[450px] lg:h-[550px]">
+        <div class="container mx-auto pt-[40px] px-4 flex flex-col md:flex-row items-center">
+            <!-- Imagen al lado izquierdo -->
+            <div class="w-full md:w-1/2 flex justify-center">
+                <img src="{{ asset('images/Logo_BAD_DOGGY.jpg') }}" style="
+                mask-image: url('{{ asset('images/Marco.png') }}');
+                -webkit-mask-image: url('{{ asset('images/Marco.png') }}');
+                mask-size: cover;
+                -webkit-mask-size: cover;
+                mask-repeat: no-repeat;
+                -webkit-mask-repeat: no-repeat;
+                mask-position: center;
+                -webkit-mask-position: center;
+                " alt="Imagen Nosotros" class="w-[500px] h-auto rounded-full shadow-lg opacity-100">
+            </div>
+            <!-- Card de Nosotros -->
+            <div class="w-full md:w-1/2 mt-6 md:mt-0 md:ml-8 bg-white bg-opacity-40 p-6 rounded-lg shadow-lg border-4 border-black border-dashed">
+                <h2 class="text-6xl text-center font-bold text-gray-800 mb-4" style="font-family: 'Fascinate', system-ui;font-weight: 400;font-style: normal;">Nuestra Manada</h2>
+                <p class="text-black text-2xl text-justify">
+                    Dicen que portarse mal no está bien... pero en
+                    <span class="relative inline-block before:absolute before:-inset-1 before:block before:-skew-y-3 before:bg-amber-500">
+                        <span class="relative text-white dark:text-black">Bad Doggy</span>
+                    </span>, ¡es delicioso!
+                    Somos fanáticos del sabor extremo y de romper la rutina con cada mordida.
+                    Aquí todo se trata de amigos, risas y comida que no se olvida.
+                    Ven, suéltate, y sé parte de la manada.
+                </p>
+            </div>
+        </div>
+    </section>
     <section id="Contactanos" style="background-image:url('{{ asset('images/Contactanos.png') }}')" class="bg-cover bg-center w-full bg-black h-48 sm:h-64 md:h-96 lg:h-screen"></section>
     <section id="Menu" style="background-image:url('{{ asset('images/Menu.png') }}')" class="bg-cover bg-center w-full bg-black h-48 sm:h-64 md:h-96 lg:h-screen"></section>
   </body>
